ec46afb304fd0f60b4e5b2c27102361da1fbaf9f
abdullah66999 <abdullahtunkaev69@gmail.com>
2026-09-17 18:49:36 +0300

    Delete projects directory

--- projects/05_csv_analytics/tests/test_analytics.py
+++
@@ -1,8 +0,0 @@
-from decimal import Decimal
-
-from analytics import Sale, totals_by_product
-
-
-def test_totals_by_product():
-    sales = [Sale("Чай", Decimal("10.00")), Sale("Чай", Decimal("2.50"))]
-    assert totals_by_product(sales)["Чай"] == Decimal("12.50")
